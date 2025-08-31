b3376ea7972ddd9bcbab85c69c0e4f18704137dd
d1zzoff <dzzoff11@gmail.com>
2025-08-31 16:11:01 +0200

    Refactor Next.js configuration to an empty state and remove wrangler.jsonc file

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["app.d1zz.xyz", "*.d1zz.xyz"],
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "app.d1zz.xyz",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
